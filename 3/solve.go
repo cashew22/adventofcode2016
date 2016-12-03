f336d23567a0ef4e9b4ee6fae0bf887d58c66df1
Eric Tremblay <habscup@gmail.com>
2016-12-03 09:16:18 -0500

    clean up

--- 3/solve.go
+++ 3/solve.go
@@ -11,26 +11,24 @@ func valid(a int, b int, c int) bool {
     return a + b > c && a + c > b && c + b > a
 }
 
-
 func main() {
     start := time.Now() //timestamp
 	fmt.Printf("Solution for day 18 GO !\n")
 
+    var valid_triangle = 0
+    var valid_triangle_day2 = 0
+    var a, b, c, a1, b1, c1, a2, b2, c2, a3 ,b3, c3 int 
+
     //Read file
     file, _ := ioutil.ReadFile("input.txt")
     data := strings.Split(string(file), "\n")
-
-    var valid_triangle = 0
-    var valid_triangle_day2 = 0
-    var a, b, c int
 
     for _, e := range data {
         fmt.Sscanf(e, "%d%d%d", &a, &b, &c)
         if valid(a, b, c) {valid_triangle++}
     }
     fmt.Printf("Q1: number of valid triangle = %d\n", valid_triangle)
-    
-    var a1, b1, c1, a2, b2, c2, a3 ,b3, c3 int 
+
     for i:=0; i<len(data); i+=3 {
         fmt.Sscanf(data[i], "%d%d%d", &a1, &a2, &a3)
         fmt.Sscanf(data[i+1], "%d%d%d", &b1, &b2, &b3)
